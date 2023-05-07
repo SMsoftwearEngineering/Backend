ddf33c886957293b8e74f14e39a9b441fb98b47a
gujaeseong <gjs5448@gmail.com>
2023-05-07 15:35:11 +0900

    TodoFolderRepository --> findByTitle 메서드 작성 TodoRepository --> findByMemberId 메서드 작성

--- src/main/java/com/example/todosoftwareengineering/repository/TodoFolderRepository.java
+++ src/main/java/com/example/todosoftwareengineering/repository/TodoFolderRepository.java
@@ -4,4 +4,5 @@ import com.example.todosoftwareengineering.domain.TodoFolderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoFolderRepository extends JpaRepository<TodoFolderEntity,Long> {
+    TodoFolderEntity findByTitle(String title);
 }
--- src/main/java/com/example/todosoftwareengineering/repository/TodoRepository.java
+++ src/main/java/com/example/todosoftwareengineering/repository/TodoRepository.java
@@ -2,6 +2,16 @@ package com.example.todosoftwareengineering.repository;
 
 import com.example.todosoftwareengineering.domain.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TodoRepository extends JpaRepository<TodoEntity,Long> {
+
+
+    //todo: 최적화 필요
+    @Query("select t from TodoEntity t " +
+            "where t.memberEntity.id = :memberId ")
+    List<TodoEntity> findByMemberId(@Param("memberId") Long memberId);
 }
