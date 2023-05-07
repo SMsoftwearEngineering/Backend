ddc4b1edd655d1a55b41e410476dda0f4ad5064c
gujaeseong <gjs5448@gmail.com>
2023-05-07 14:30:57 +0900

    TodoFolderPostMapper,TodoFolderResponseMapper 작성

---
+++ src/main/java/com/example/todosoftwareengineering/mapper/todofolder/TodoFolderResponseMapper.java
@@ -0,0 +1,18 @@
+package com.example.todosoftwareengineering.mapper.todofolder;
+
+import com.example.todosoftwareengineering.domain.TodoEntity;
+import com.example.todosoftwareengineering.domain.TodoFolderEntity;
+import com.example.todosoftwareengineering.dto.TodoDto;
+import com.example.todosoftwareengineering.dto.TodoFolderDto;
+import com.example.todosoftwareengineering.mapper.GenericMapper;
+import org.mapstruct.Mapper;
+
+/**
+ * @Mapper : MapStruct Code Generator가 해당 인터페이스의 구현체를 생성해준다.
+ * componentModel = "spring" : spring에 맞게 bean으로 등록해준다
+ */
+@Mapper(componentModel = "spring")
+public interface TodoFolderResponseMapper extends GenericMapper<TodoFolderDto.TodoFolderResponseDto, TodoFolderEntity> {
+
+
+}
